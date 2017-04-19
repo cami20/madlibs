0e9ffb045fb844699423229e88e0434b292214d2
Hackbright Student <no-reply@hackbrightacademy.com>
2017-04-18 18:02:13 -0700

    madlibs with some style

--- madlibs.py
+++ madlibs.py
@@ -1,8 +1,8 @@
 """A madlib game that compliments its users."""
 
 from random import choice
-
 from flask import Flask, render_template, request
+
 
 # "__name__" is a special Python variable for the name of the current module.
 # Flask wants to know this to know what any imported things are relative to.
@@ -19,20 +19,7 @@ AWESOMENESS = [
 def start_here():
     """Display homepage."""
 
-
-
     return render_template("gotohello.html")
-    #  <!doctype html>
-    #  <html>
-    #     <head>
-    #         <title></title>
-    #     </head>
-    #     <body>
-    #         <a href="/hello">Hello! This is the home page.</a>
-    #     </body>
-    #  </html>
-
-    # """
 
 
 @app.route('/hello')
